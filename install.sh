9610ee979a36aec60b324f62745cfe385d5bd388
major <major.goo@autocore.ai>
2023-06-05 13:53:02 +0800

    fix env ZSH_CUSTOM

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ echo "-----> Installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/gmingj/ohmyzsh/myohmyzsh/tools/install.sh)"
 
 echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
